999abad681c9851e5da78c3bb3075c7a0a019ab6
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-07-24 19:47:44 +0100

    Add argument parsing macro
    Macro to convert arguments into IDs to add to the cache hashmap (in MemCache struct)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap; // hashmap of cached args and results
 use std::fmt::Display; // box cache results of any type
 use std::mem::size_of_val; // get current cache size
+use std::collections::hash_map::DefaultHasher; // convert args to id
+use std::hash::{Hash, Hasher}; // convert args to id
 
 /// Struct to determine how to cache results
 /// in memory (faster)
@@ -9,4 +11,22 @@ pub struct MemCache {
     hits: u32,
     misses: u32,
     cache: HashMap<String, Box<dyn Display + 'static>>,
+}
+
+macro_rules! args_id {
+    ($($x:expr), *) => {{
+        let mut s = DefaultHasher::new();
+        let mut v: Vec<u64> = Vec::new();
+        $(
+            v.push({
+                $x.hash(&mut s);
+                s.finish()
+            });
+        )*
+
+        {
+            v.hash(&mut s);
+            s.finish()
+        }
+    };}
 }
