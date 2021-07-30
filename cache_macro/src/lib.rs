de94a82455d329bcf23d3a4c4d7e91ddd1ecdbe5
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-07-30 20:28:04 +0100

    Fix parsing metadata
    Instead of cloning, directly reference it in the main attribute macro

--- cache_macro/src/lib.rs
+++ cache_macro/src/lib.rs
@@ -32,14 +32,21 @@ pub fn test_macro(metadata: TokenStream, item: TokenStream) -> TokenStream {
     // Parse the caching struct - convert from TokenStream to an Item
     // or a TokenStream to an Args struct
     // let cache_struct: syn::Item = syn::parse(metadata).expect("failed to parse");
-    let cache_struct = parse_macro_input!(metadata as Args);
+    let cache_hs: HashSet<Ident> = parse_macro_input!(metadata as Args).vars;
+
+    // Get a reference to the cache_struct Ident (only value in cache_hs)
+    let cache_ident: &Ident = match cache_hs.iter().next() {
+        Some(x) => { x },
+        None => { panic!("no cache argument") }
+    };
+
     let input_fn: Item = syn::parse(item).expect("failed to parse input");
 
     // Ensure input_fn is a function
     match input_fn {
         Item::Fn(ref fn_item) => {
             // Is a function, get the arguments
-
+            let argument_id = encode_arguments(fn_item);
         },
         
         _ => {
@@ -47,15 +54,21 @@ pub fn test_macro(metadata: TokenStream, item: TokenStream) -> TokenStream {
         }
     }
 
+    // Add code to accomodate for cache
+
+
     // Convert input_fn (syn::Item) back into TokenStream
     // and return it
     quote!(#input_fn).into()
 }
 
+fn encode_arguments(struct_: &syn::ItemFn) -> u64 {
+    1u64
+}
+
 macro_rules! args_id { 
     ($($x:expr), *) => {{
         let mut s = DefaultHasher::new();
-        let mut v: Vec<u64> = Vec::new();
         $(
             $x.hash(&mut s);
         )*
