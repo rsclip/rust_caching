64432cb41833d8761a30f506865a0ab1356acd7f
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-07-31 21:35:24 +0100

    Create cache macros

--- src/lib.rs
+++ src/lib.rs
@@ -28,4 +28,64 @@ impl MemCache {
     pub fn check_cache(&self) -> Option<i32> {
         None
     }
+
+    /// Register a hit
+    fn hit(&mut self) {
+        self.hits += 1;
+    }
+
+    /// Register a miss
+    fn miss(&mut self) {
+        self.misses += 1;
+    }
+}
+
+/// Main macro to cache a section of code, ideally used with args! macro
+/// i.e `cache!(cache_struct, args!(1, 2, 3))`
+macro_rules! cache {
+    ($s:expr, $a:expr) => {
+        // $s: cache struct
+        // $a: argument id
+        let r = $s.check_cache($a);
+        match r {
+            std::option::Option::Some(cached_result) => {
+                $s.hit();
+                Some(cached_result)
+            },
+
+            std::option::Option::None => {
+                $s.miss();
+                None
+            }
+        }
+    }
+}
+
+/// Get the current running function
+/// Caching should be applied to an entire function generally
+/// but it is possible to cache a section of code.
+macro_rules! function {
+    () => {{
+        fn f() {}
+        fn type_name_of<T>(_: T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+        let name = type_name_of(f);
+        &name[..name.len() - 3]
+    }}
+}
+
+/// Convert the input argument variables into a hashed ID
+/// i.e `args!(Struct{}, 30)`
+macro_rules! args {
+    ($($x:expr), *) => {{
+        let mut s = DefaultHasher::new();
+        $(
+            $x.hash(&mut s);
+        )*
+
+        function!().hash(&mut s);
+
+        s.finish()
+    };}
 }
