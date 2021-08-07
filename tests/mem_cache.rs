4c42940eec5fe2ae54946b0bbc254b47847de0f5
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-08-07 19:41:56 +0100

    Make "Memory" module
    Separate file and memory

--- tests/mem_cache.rs
+++ tests/mem_cache.rs
@@ -1,6 +1,6 @@
 use rust_caching::*;
 
-fn expensive_fn(cache: &mut MemCache, num: i32) -> i32 {
+fn expensive_fn(cache: &mut Memory::MemCache, num: i32) -> i32 {
     check_cache!(cache, args!(num), i32, {
         // Function code here
         4i32
@@ -11,7 +11,7 @@ fn expensive_fn(cache: &mut MemCache, num: i32) -> i32 {
 fn main() {
     let cache_size = 10;
     println!("Cache size {}", cache_size);
-    let mut cache = MemCache::new(cache_size);
+    let mut cache = Memory::MemCache::new(cache_size);
     for i in 1..20 {
         for _ in 0..2 {
             expensive_fn(&mut cache, i as i32);
